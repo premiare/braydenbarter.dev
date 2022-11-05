f16713e91403f59c5437efc29e336b1c69b0c796
premiare <braydenbarter@gmail.com>
2022-11-05 22:49:34 +1100

    fix tech color classes.

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -26,19 +26,44 @@ type TechIcons = {
   [key in TechType]: JSX.Element;
 };
 
-const TECH_ICONS: TechIcons = {
-  next: { icon: <SiNextdotjs />, color: "#111111", name: "Next.js" },
-  react: <SiReact />,
-  node: <SiNodedotjs />,
-  typescript: <SiTypescript />,
-  javascript: <SiJavascript />,
-  html: <SiHtml5 />,
-  css: <SiCss3 />,
-  sass: <SiSass />,
-  tailwind: <SiTailwindcss />,
-  bootstrap: <SiBootstrap />,
-  supabase: <SiSupabase />,
-  mantine: <SiStyledcomponents />,
+const TECH: TechIcons = {
+  next: { icon: <SiNextdotjs />, name: "Next.js", class: "text-next" },
+  react: { icon: <SiReact />, name: "React", class: "text-react" },
+  node: { icon: <SiNodedotjs />, name: "Node.js", class: "text-node" },
+  typescript: {
+    icon: <SiTypescript />,
+    name: "TypeScript",
+    class: "text-typescript",
+  },
+  javascript: {
+    icon: <SiJavascript />,
+    name: "JavaScript",
+    class: "text-javascript",
+  },
+  jquery: { icon: <SiJquery />, name: "jQuery", class: "text-jquery" },
+  html: { icon: <SiHtml5 />, name: "HTML", class: "text-html" },
+  css: { icon: <SiCss3 />, name: "CSS", class: "text-css" },
+  tailwind: {
+    icon: <SiTailwindcss />,
+    name: "Tailwind",
+    class: "text-tailwind",
+  },
+  bootstrap: {
+    icon: <SiBootstrap />,
+    name: "Bootstrap",
+    class: "text-bootstrap",
+  },
+  supabase: { icon: <SiSupabase />, name: "Supabase", class: "text-supabase" },
+  mantine: {
+    icon: <SiStyledcomponents />,
+    name: "Mantine",
+    class: "text-mantine",
+  },
+  styledComponents: {
+    icon: <SiStyledcomponents />,
+    name: "Styled Components",
+    class: "text-styledComponents",
+  },
 };
 
 const ProjectCard = ({ project, alt }) => {
@@ -50,26 +75,29 @@ const ProjectCard = ({ project, alt }) => {
       )}
     >
       <div className="flex flex-col w-[60%] h-100 gap-4">
-        <div className="text-white text-3xl">{project.title}</div>
+        <div className="text-react text-3xl">{project.title}</div>
         <div className="text-white text-md">{project.description}</div>
-        <div className="flex flex-row gap-8 w-100 mx-auto mt-4 h-auto p-8">
-          {project.tech.map((t: string) => (
-            <>
-              <div className="flex flex-col justify-center items-center group relative">
-                <div
-                  key={t}
-                  className={`text-4xl hover:text-[${TECH_ICONS[t].color}] text-[${TECH_ICONS[t].color}] transition p-2`}
-                >
-                  {TECH_ICONS[t].icon}
+        <div className="flex flex-row gap-8 w-100 mx-auto mt-4 h-auto p-8 flex-wrap">
+          {project.tech.map((t: string) => {
+            console.log(t);
+            return (
+              <>
+                <div className="flex flex-col justify-center items-center group relative">
+                  <div
+                    key={t}
+                    className={`text-4xl ${TECH[t].class} transition p-2`}
+                  >
+                    {TECH[t].icon}
+                  </div>
+                  <span className="text-white opacity-0 group-hover:opacity-100 text-md absolute -bottom-6 text-lg text-center transition-all bg-transparent border px-4 h-100 w-100 flex justify-center items-center align-middle mx-auto rounded-md">
+                    {TECH[t].name}
+                  </span>
                 </div>
-                <span className="text-white opacity-0 group-hover:opacity-100 text-md absolute -bottom-5 text-lg text-center transition-all ">
-                  {t}
-                </span>
-              </div>
-            </>
-          ))}
+              </>
+            );
+          })}
         </div>
-        <div className="flex flex-row gap-2 w-100 mx-auto mt-2">
+        <div className="flex flex-row flex-wrap gap-2 w-100 mx-auto mt-2">
           <Link
             href={project.link}
             target="_blank"
@@ -88,12 +116,13 @@ const ProjectCard = ({ project, alt }) => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row mx-auto justify-center">
+      <div className="flex flex-row mx-auto justify-center relative">
         <Image
+          className="object-cover overflow-hidden"
           src={project.image}
           alt={project.title}
-          width={300}
-          height={300}
+          width={200}
+          height={225}
         />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        react: "#61dafb",
+        next: "#000",
+        tailwind: "#38bdf8",
+        mantine: "#339af0",
+        styledComponents: "#d874b1",
+        supabase: "#f56565",
+        typescript: "#3178c6",
+        javascript: "#f7df1e",
+        node: "#339933",
+        html: "#e34c26",
+        css: "#264de4",
+        sass: "#cc6699",
+        bootstrap: "#563d7c",
+        jquery: "#0769ad",
+      },
       fontFamily: {
         sans: ["Manrope", "sans-serif"],
       },
