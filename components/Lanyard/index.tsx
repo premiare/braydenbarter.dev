e8f7c8cbcbf683c9bf91f42de98e38404be81e77
premiare <braydenbarter@gmail.com>
2022-11-22 22:04:44 +1100

    fix song playing responsiveness

--- components/Lanyard/index.tsx
+++ components/Lanyard/index.tsx
@@ -6,14 +6,24 @@ export const SpotifyInfo = () => {
   const { spotify } = Lanyard();
   if (!spotify) return null;
   const newArtist = spotify.artist.split(";").join(", ");
+
+  const isSmall = () => {
+    return window.innerWidth < 640;
+  };
+
   return (
     <>
       <div className="flex flex-row items-center gap-2">
         <SiSpotify className="text-green-500" />
-        <div className="flex flex-col">
-          <p className="truncate">{spotify && spotify.song}</p>
-          <p className="truncate">{spotify && newArtist}</p>
+
+        <div className="flex flex-col sm:flex-row ">
+          <p className="flex sm:hidden truncate">{spotify && spotify.song}</p>
+          <p className="flex sm:hidden truncate">{spotify && newArtist}</p>
+          <p className="hidden sm:flex truncate">
+            {spotify && spotify.song} - {spotify && newArtist}
+          </p>
         </div>
+
         <div className="h-8 w-8 rounded-full animate-spin-slow">
           <img
             src={spotify.album_art_url}
